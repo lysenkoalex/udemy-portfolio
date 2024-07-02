5d7b31039bce4b3c385462e3c7d3fc0fea1cb3b3
Oleksandr Lysenko <alexsmith.ua@gmail.com>
2024-07-03 00:12:10 +0300

    initial commit

---
+++ content/blog/2022/first.md
@@ -0,0 +1,4 @@
+---
+publishedAt: 2023-07-12 17:15:00
+---
+# First post in our website in 2022
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss', 
+    '@nuxtjs/color-mode', 
+    "@nuxt/content"
+  ],
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    }
+  },
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
+  colorMode: {
+    classSuffix: ''
+  },
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml']
+    }
+  }
+})
